e551179d5fa8ccffe0f7aa461df7964e0078b018
AndrewDevos <48452066+DevosKaizen@users.noreply.github.com>
2024-08-06 21:05:34 -0300

    main

--- client.py
+++ client.py
@@ -2,40 +2,57 @@ import socket
 import threading
 import time
 
+# Definindo a porta de comunicação e o formato de codificação das mensagens
 PORT = 5050
 FORMATO = 'utf-8'
-SERVER = "10.230.141.60"
+# Endereço IP do servidor
+SERVER = "192.168.88.12"
+# Endereço completo (IP e porta) do servidor
 ADDR = (SERVER, PORT)
 
+# Criando o socket do cliente usando IPv4 (AF_INET) e TCP (SOCK_STREAM)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Conectando o cliente ao servidor especificado
 client.connect(ADDR)
 
+# Função para lidar com as mensagens recebidas do servidor
 def handle_mensagens():
     while True:
+        # Recebe a mensagem do servidor
         msg = client.recv(1024).decode()
+        # Divide a mensagem em partes usando "=" como delimitador
         mensagem_splitada = msg.split("=")
+        # Imprime a mensagem no formato "nome: mensagem"
         print(mensagem_splitada[1] + ": " + mensagem_splitada[2])
 
+# Função para enviar uma mensagem codificada para o servidor
 def enviar(mensagem):
     client.send(mensagem.encode(FORMATO))
 
+# Função para ler uma mensagem do usuário e enviá-la ao servidor
 def enviar_mensagem():
     mensagem = input()
     enviar("msg=" + mensagem)
 
+# Função para ler o nome do usuário e enviá-lo ao servidor
 def enviar_nome():
     nome = input('Digite seu nome: ')
     enviar("nome=" + nome)
 
+# Função para iniciar o envio de mensagens
 def iniciar_envio():
-    enviar_nome()
+    enviar_nome()  # Primeiro, envia o nome do usuário
     while True:
-        enviar_mensagem()
+        enviar_mensagem()  # Em seguida, envia mensagens continuamente
 
+# Função principal para iniciar o cliente
 def iniciar():
+    # Cria e inicia uma thread para lidar com as mensagens recebidas
     thread1 = threading.Thread(target=handle_mensagens)
+    # Cria e inicia uma thread para lidar com o envio de mensagens
     thread2 = threading.Thread(target=iniciar_envio)
     thread1.start()
     thread2.start()
 
+# Inicia o cliente
 iniciar()
--- server.py
+++ server.py
@@ -2,71 +2,92 @@ import socket
 import threading
 import time
 
-SERVER_IP = "10.230.141.60"
-PORT = 5050
-ADDR = (SERVER_IP, PORT)
-FORMATO = 'utf-8'
+# Configurações do servidor
+HOST = '127.0.0.1'
+PORT = 12345
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+server.bind((HOST, PORT))
 
+# Listas para armazenar conexões e mensagens
 conexoes = []
 mensagens = []
+clientes = {}
 
-def enviar_mensagem_individual(conexao):
-    print(f"[ENVIANDO] Enviando mensagens para {conexao['addr']}")
-    for i in range(conexao['last'], len(mensagens)):
-        mensagem_de_envio = "msg=" + mensagens[i]
-        conexao['conn'].send(mensagem_de_envio.encode())
-        conexao['last'] = i + 1
-        time.sleep(0.2)
+# Função para enviar mensagens antigas para um cliente específico
+def enviar_mensagem_individual(cliente):
+    for mensagem in mensagens:
+        if mensagem['dst'] == cliente['id']:
+            cliente['conn'].send(mensagem['data'].encode())
 
+# Função para enviar uma mensagem para todos os clientes
 def enviar_mensagem_todos():
-    global conexoes
-    for conexao in conexoes:
-        enviar_mensagem_individual(conexao)
+    for cliente in conexoes:
+        for mensagem in mensagens:
+            cliente['conn'].send(mensagem['data'].encode())
 
-"""
-1 vez que o cliente entrar, vai mandar o nome:
-nome=.....
-E as mensagens vem:
-msg=
-"""
-
+# Função para lidar com clientes
 def handle_clientes(conn, addr):
-    print(f"[NOVA CONEXAO] Um novo usuario se conectou pelo endereço {addr}")
-    global conexoes
-    global mensagens
-    nome = False
-
-    while(True):
-        msg = conn.recv(1024).decode(FORMATO)
-        if(msg):
-            if(msg.startswith("nome=")):
-                mensagem_separada = msg.split("=")
-                nome = mensagem_separada[1]
+    print(f"[NOVA CONEXÃO] {addr} conectado.")
+    nome = conn.recv(1024).decode()
+    
+    while True:
+        msg = conn.recv(1024).decode()
+        if not msg:
+            break
+        
+        # Registro de cliente
+        if msg.startswith("01"):
+            cliente_id = str(len(clientes) + 1).zfill(13)
+            clientes[cliente_id] = conn
+            conn.send(f"02{cliente_id}".encode())
+        
+        # Conexão de cliente
+        elif msg.startswith("03"):
+            cliente_id = msg[2:15]
+            if cliente_id in clientes:
                 mapa_da_conexao = {
                     "conn": conn,
                     "addr": addr,
-                    "nome": nome,
+                    "id": cliente_id,
                     "last": 0
                 }
                 conexoes.append(mapa_da_conexao)
                 enviar_mensagem_individual(mapa_da_conexao)
-            elif(msg.startswith("msg=")):
-                mensagem_separada = msg.split("=")
-                mensagem = nome + "=" + mensagem_separada[1]
-                mensagens.append(mensagem)
-                enviar_mensagem_todos()
+        
+        # Envio de mensagem
+        elif msg.startswith("05"):
+            src = msg[2:15]
+            dst = msg[15:28]
+            timestamp = msg[28:38]
+            data = msg[38:]
+            mensagem = {
+                "src": src,
+                "dst": dst,
+                "timestamp": timestamp,
+                "data": data
+            }
+            mensagens.append(mensagem)
+            for cliente in conexoes:
+                if cliente['id'] == dst:
+                    cliente['conn'].send(f"06{src}{dst}{timestamp}{data}".encode())
+                    break
+        
+        # Mensagem de chat (antigo)
+        elif msg.startswith("msg="):
+            mensagem_separada = msg.split("=")
+            mensagem = nome + "=" + mensagem_separada[1]
+            mensagens.append(mensagem)
+            enviar_mensagem_todos()
 
-
-
+# Função para iniciar o servidor
 def start():
     print("[INICIANDO] Sockets com python")
     server.listen()
-    while(True):
+    while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_clientes, args=(conn, addr))
         thread.start()
 
+# Inicia o servidor
 start()
